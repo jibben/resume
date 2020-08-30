7a90aa0ce52ddca78374c891c6f4db49e113a330
Jibben Hillen <jibbenh@gmail.com>
2020-08-30 11:55:07 -0400

    keep pdf around

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 xelatex jibben_hillen.tex && \
 gsutil cp jibben_hillen.pdf gs://jibben-resume && \
-rm jibben_hillen.aux jibben_hillen.log jibben_hillen.out jibben_hillen.pdf
+rm jibben_hillen.aux jibben_hillen.log jibben_hillen.out
